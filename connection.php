cad2721d1813eb362f1c38f8bef8e63b6c6d2f87
laysonaljon <80148930+laysonaljon@users.noreply.github.com>
2023-12-17 05:38:25 +0800

    Update DB variables connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-	DEFINE("DB_SERVER", "localhost");
-	DEFINE("DB_USERNAME", "root");
-	DEFINE("DB_PASSWORD", "");
-	DEFINE("DB_NAME", "new_event");
+	DEFINE("DB_SERVER", "laundryandwash.com");
+	DEFINE("DB_USERNAME", "phpxffcj_laundry");
+	DEFINE("DB_PASSWORD", "JkH]!4c7P)V?");
+	DEFINE("DB_NAME", "phpxffcj_event");
 
 	function openConnection(){
 		$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
