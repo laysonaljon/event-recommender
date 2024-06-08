203b36155ad6c4cc357b712da733e31e4b19e4f9
Aljon Layson <aljonlayson21@gmail.com>
2024-06-09 01:00:42 +0800

    Improve Sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,36 +1,39 @@
-<?php
-    include 'session.php';
-?>
+<?php include 'session.php';?>
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="http://localhost/event-recommender/js/tailwind.config.js"></script>
 <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
-                <div class="sidebar-brand-text mx-3">DASHBOARD</div>
-            </a>
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+        <i class="fab fa-buromobelexperte"></i>
+        <div class="sidebar-brand-text mx-3">DASHBOARD</div>
+    </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="event-add.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Add Event</span>
-                </a>
-            </li>
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="event-add.php">
+        <i class="fas fa-calendar-plus"></i>
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="event.php">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Event</span>
-                </a>
-            </li>
+            <span>Add Event</span>
+        </a>
+    </li>
 
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-        </ul>
-        <!-- End of Sidebar -->
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="event.php">
+            <i class="fas fa-calendar-alt"></i>
+            <span>Event</span>
+        </a>
+    </li>
+
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
+<!-- End of Sidebar -->
